a5b1f40b13036d2fef829235d8551fe9fda93c22
Nikhil Reddy Pannala <77602266+Nikhilreddy012@users.noreply.github.com>
2021-03-17 12:34:22 -0500

    Added table in app2

--- app2.js
+++ app2.js
@@ -11,9 +11,13 @@ const server =
 
       res.write('<html> <head> <title> Served </title> </head>')
       res.write('<body>')
-      res.write('Content \n')
-      res.write('More content \n')
-      res.write('Hello World')    
+      res.write('<table>')
+      res.write('<tr><th>Location</th><th>Time</th><th>Money</th></tr>')
+      res.write(' <tr><td>Paris</td><td>72 hr</td><td>2500$</td></tr>')
+      res.write(' <tr><td>London</td><td>48 hr</td><td>2000$</td></tr>')
+      res.write(' <tr><td>Sweden</td><td>48 hr</td><td>1500$</td></tr>')
+      res.write(' <tr><td>Delhi</td><td>24 hr</td><td>1000$</td></tr>')
+      res.write('</table>')
       res.end('</body></html>')
       //Close the response
     }                           
